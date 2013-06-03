d5fe36538314a68c062bd413c879bc3459f3f10c
Jeanderson Barros Candido <jeandersonbc@gmail.com>
2013-06-03 15:48:13 -0400

    Improved path element

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -8,23 +8,37 @@
 #>
 
 # ALIASES
+#
+# Put your alias here. For instance "set-alias myAlias Real_Name"
 set-alias vi gvim
 set-alias vim gvim
 
 # PATH
-$env:PATH = $env:PATH + ";C:\Program Files\SlikSvn\bin\"
+#
+# FIXME: Hardcoding... NOT COOL! What about file reading?
+$path_elements = @(
+	"C:\Users\Jeanderson\Dev\apache-ant-1.9.1\bin";
+	"C:\Program Files\SlikSvn\bin\";
+)
+foreach ($element in $path_elements) {
+	$env:PATH = $env:PATH + $element + ";"
+}
 
 # .DESCRIPTION
 #     Changes the prompt line to ``hh:mm computerName:[../relativePath]>``
 function prompt {
     $rel_path = Resolve-Path . -Relative
-    $color1 = [ConsoleColor]::DarkCyan
-    $color2 = [ConsoleColor]::Cyan
 
-    Write-Host $(get-date).toString("hh:mm ") -foregroundColor $color1 -noNewLine
-    Write-Host ($env:ComputerName + ":[") -foregroundColor $color1 -noNewLine
-    Write-Host $rel_path -foregroundColor $color2 -noNewLine
-    Write-Host "]>" -foregroundColor $color1 -noNewLine
+	# Set your custom colors here
+    $prompt_color = [ConsoleColor]::DarkCyan
+    $path_color = [ConsoleColor]::Cyan
+	$text_color = [ConsoleColor]::Green
+
+    Write-Host $(get-date).toString("hh:mm ") -foregroundColor $prompt_color -noNewLine
+    Write-Host ($env:ComputerName + ":[") -foregroundColor $prompt_color -noNewLine
+    Write-Host $rel_path -foregroundColor $path_color -noNewLine
+    Write-Host "]>" -foregroundColor $prompt_color -noNewLine
+	Write-Host -foregroundColor $text_color -noNewLine
 
     return " "
 }
@@ -39,4 +53,13 @@ function newfile {
     foreach($filename in $args) {
         new-item -Path . -itemtype "file" -name $filename
     }
+}
+
+# TODO
+# if path exists, adds to $path_elements list
+#
+# figure out a way to update PATH or open a new PS session in the current
+# directory
+function addPath {
+    Write-Host "Not implemented yet..." -foregroundColor ([ConsoleColor]::Red)
 }
