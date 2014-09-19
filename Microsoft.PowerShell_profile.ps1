2beb4d586d4474d42738adad8fc7aada19d6e537
Jeanderson Candido <jeandersonbc@gmail.com>
2014-09-19 02:03:06 -0300

    Created git function to update all projects according to a regex

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -79,3 +79,16 @@ function newfiles {
 function admin($program) {
 	start-process $program -verb runas
 }
+
+# Honestly, life is too short to update every single git project manually...
+function gitupdate($projectDirRegex) {
+    foreach ($dir in dir) {
+        if ($dir.Name.contains($projectDirRegex) -and $dir.Attributes.ToString().Equals("Directory")) {
+            cd $dir;
+            Write-Host "Pulling changes for '$dir'..." -ForegroundColor Green
+            git pull;
+            cd ..;
+        }
+    }
+    Write-Host "Done!" -ForegroundColor Green
+}
